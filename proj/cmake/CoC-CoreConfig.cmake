9d55baa27cfb0dfe77fcdda1e520f5fde6c07495
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-03-06 13:57:20 +1100

    update cmake file

--- proj/cmake/CoC-CoreConfig.cmake
+++ proj/cmake/CoC-CoreConfig.cmake
@@ -1,17 +1,14 @@
-set(BLOCK_NAME "CoC-Core")
+if( NOT TARGET CoC-Core )
+	get_filename_component( COC-CORE_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE )
 
-if( NOT TARGET ${BLOCK_NAME} )
-	get_filename_component( BLOCK_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE )
+	FILE(GLOB COC-CORE_SOURCES ${COC-CORE_SOURCE_PATH}/*.cpp)
 
-	FILE(GLOB BLOCK_SOURCES ${BLOCK_SOURCE_PATH}/*.cpp)
-
-	add_library( ${BLOCK_NAME} ${BLOCK_SOURCES} )
-	target_include_directories( ${BLOCK_NAME} PUBLIC "${BLOCK_SOURCE_PATH}" )
-
+	add_library( CoC-Core ${COC-CORE_SOURCES} )
+	target_include_directories( CoC-Core PUBLIC "${COC-CORE_SOURCE_PATH}" )
 
 	# If Cinder included from this block:
 
-	target_include_directories( ${BLOCK_NAME} SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+	target_include_directories( CoC-Core SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
 
 	if( NOT TARGET cinder )
 		include( "${CINDER_PATH}/proj/cmake/configure.cmake" )
@@ -20,11 +17,6 @@ if( NOT TARGET ${BLOCK_NAME} )
 				"$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}" )
 	endif()
 
-	target_link_libraries( ${BLOCK_NAME} PRIVATE cinder )
-	
+	target_link_libraries( CoC-Core PRIVATE cinder )
+
 endif()
-
-
-
-
-
